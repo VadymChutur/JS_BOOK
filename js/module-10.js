aab3e4fc3e4f21c103ed9c066fa2c79f81628574
Vadym Chutur <falloutgreck@gmail.com>
2024-01-31 16:31:42 +0200

    upd

--- js/module-10.js
+++ js/module-10.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 // ctx.fillRect(0, 0, 10, 10);
 
+ctx.fillStyle = 'red';
 ctx.fillRect(60, 0, 50, 50);
 for (let i = 0; i < 7; i += 1) {
   ctx.fillRect(80, i * 10, 10, 10);
@@ -16,3 +17,25 @@ for (let i = 7; i < 20; i += 1) {
 for (let i = 7; i < 20; i += 1) {
   ctx.fillRect(110, i * 10, 10, 10);
 }
+
+ctx.fillStyle = '#9ACD32';
+ctx.fillRect(0, 200, 50, 100);
+
+ctx.fillStyle = '#FFA500';
+ctx.fillRect(50, 200, 50, 100);
+
+ctx.fillStyle = '#00FF00';
+ctx.fillRect(100, 200, 50, 100);
+
+ctx.fillStyle = 'black';
+ctx.lineWidth = 4;
+ctx.strokeRect(0, 200, 150, 100);
+
+ctx.strokeStyle = 'Turquoise';
+ctx.lineWidth = 4;
+ctx.beginPath();
+ctx.moveTo(210, 10);
+ctx.lineTo(260, 60);
+ctx.moveTo(260, 10);
+ctx.lineTo(210, 60);
+ctx.stroke();
