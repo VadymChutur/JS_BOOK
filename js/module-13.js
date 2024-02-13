4d19a03a4b5b37a602b7711e69a8b6af2323dfcb
Vadym Chutur <falloutgreck@gmail.com>
2024-02-13 15:54:05 +0200

    Update module-13.js

--- js/module-13.js
+++ js/module-13.js
@@ -1,8 +1,10 @@
 const Ball = function () {
-  this.x = 400;
-  this.y = 400;
-  this.xSpeed = -4;
-  this.ySpeed = 6;
+  this.x = width / 2;
+  this.y = heght / 2;
+  this.xSpeed = Math.random() * 10 - 5;
+  this.ySpeed = Math.random() * 10 - 5;
+  const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+  this.color = pickRandomColor(colors);
 };
 
 const circle = function (x, y, radius, fillCircle) {
@@ -15,8 +17,18 @@ const circle = function (x, y, radius, fillCircle) {
   }
 };
 
+const pickRandomColor = function (color) {
+  return color[Math.floor(Math.random() * color.length)];
+};
+
 Ball.prototype.draw = function () {
-  circle(this.x, this.y, 10, true);
+  ctx.fillStyle = this.color;
+  // console.log(this.color());
+  circle(this.x, this.y, 5, true);
+};
+
+Ball.prototype.fill = function () {
+  ctx.fillStyle = this.color();
 };
 
 Ball.prototype.move = function () {
@@ -25,24 +37,40 @@ Ball.prototype.move = function () {
 };
 
 Ball.prototype.checkCollision = function () {
-  if (this.x < 0 || this.x > 800) {
+  if (this.x < 0 || this.x > width) {
     this.xSpeed = -this.xSpeed;
+    console.log(this.xSpeed);
   }
-  if (this.y < 0 || this.y > 800) {
+  if (this.y < 0 || this.y > heght) {
     this.ySpeed = -this.ySpeed;
   }
 };
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+let width = canvas.width;
+let heght = canvas.height;
+heght = 200;
+width = 400;
+// console.log(heght);
 
-const ball = new Ball();
+const balls = [];
+for (let i = 0; i < 10; i += 1) {
+  // ball[i] = new Ball();
+  balls.push(new Ball());
+}
 
 setInterval(function () {
-  ctx.clearRect(0, 0, 800, 800);
+  ctx.clearRect(0, 0, width + 10, heght + 10);
 
-  ball.draw();
-  ball.move();
-  ball.checkCollision();
-  ctx.strokeRect(0, 0, 800, 800);
+  for (let i = 0; i < balls.length; i += 1) {
+    // console.log(ball);
+    // console.log(ball[i].draw());
+    balls[i].draw();
+    balls[i].move();
+    balls[i].checkCollision();
+    // ball[i].fill();
+  }
+
+  ctx.strokeRect(0, 0, width, heght);
 }, 30);
